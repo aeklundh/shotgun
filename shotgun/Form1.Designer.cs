d1a54d616fad9fb0dae4776a22653b3f161b117d
aeklundh <eklundh.albin@gmail.com>
2016-09-08 15:36:30 +0200

    first commit, work in progress

--- shotgun/Form1.Designer.cs
+++ shotgun/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace shotgun
 {
-    partial class Form1
+    partial class frmShotgun
     {
         /// <summary>
         /// Required designer variable.
@@ -28,12 +28,136 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.btnBlock = new System.Windows.Forms.Button();
+            this.btnFire = new System.Windows.Forms.Button();
+            this.lblPlayerAmmoCount = new System.Windows.Forms.Label();
+            this.lblComputerAmmoCount = new System.Windows.Forms.Label();
+            this.lblComputerAmmo = new System.Windows.Forms.Label();
+            this.lblPlayerAmmo = new System.Windows.Forms.Label();
+            this.lblEventLog = new System.Windows.Forms.Label();
+            this.btnShotgun = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(28, 96);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(75, 23);
+            this.btnLoad.TabIndex = 0;
+            this.btnLoad.Text = "Load";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // btnBlock
+            // 
+            this.btnBlock.Location = new System.Drawing.Point(109, 96);
+            this.btnBlock.Name = "btnBlock";
+            this.btnBlock.Size = new System.Drawing.Size(75, 23);
+            this.btnBlock.TabIndex = 1;
+            this.btnBlock.Text = "Block";
+            this.btnBlock.UseVisualStyleBackColor = true;
+            this.btnBlock.Click += new System.EventHandler(this.btnBlock_Click);
+            // 
+            // btnFire
+            // 
+            this.btnFire.Enabled = false;
+            this.btnFire.Location = new System.Drawing.Point(190, 96);
+            this.btnFire.Name = "btnFire";
+            this.btnFire.Size = new System.Drawing.Size(75, 23);
+            this.btnFire.TabIndex = 2;
+            this.btnFire.Text = "Fire";
+            this.btnFire.UseVisualStyleBackColor = true;
+            this.btnFire.Click += new System.EventHandler(this.btnFire_Click);
+            // 
+            // lblPlayerAmmoCount
+            // 
+            this.lblPlayerAmmoCount.AutoSize = true;
+            this.lblPlayerAmmoCount.Location = new System.Drawing.Point(248, 214);
+            this.lblPlayerAmmoCount.Name = "lblPlayerAmmoCount";
+            this.lblPlayerAmmoCount.Size = new System.Drawing.Size(24, 13);
+            this.lblPlayerAmmoCount.TabIndex = 3;
+            this.lblPlayerAmmoCount.Text = "0/3";
+            // 
+            // lblComputerAmmoCount
+            // 
+            this.lblComputerAmmoCount.AutoSize = true;
+            this.lblComputerAmmoCount.Location = new System.Drawing.Point(248, 239);
+            this.lblComputerAmmoCount.Name = "lblComputerAmmoCount";
+            this.lblComputerAmmoCount.Size = new System.Drawing.Size(24, 13);
+            this.lblComputerAmmoCount.TabIndex = 4;
+            this.lblComputerAmmoCount.Text = "0/3";
+            // 
+            // lblComputerAmmo
+            // 
+            this.lblComputerAmmo.AutoSize = true;
+            this.lblComputerAmmo.Location = new System.Drawing.Point(129, 239);
+            this.lblComputerAmmo.Name = "lblComputerAmmo";
+            this.lblComputerAmmo.Size = new System.Drawing.Size(111, 13);
+            this.lblComputerAmmo.TabIndex = 5;
+            this.lblComputerAmmo.Text = "Computer ammunition:";
+            // 
+            // lblPlayerAmmo
+            // 
+            this.lblPlayerAmmo.AutoSize = true;
+            this.lblPlayerAmmo.Location = new System.Drawing.Point(129, 214);
+            this.lblPlayerAmmo.Name = "lblPlayerAmmo";
+            this.lblPlayerAmmo.Size = new System.Drawing.Size(95, 13);
+            this.lblPlayerAmmo.TabIndex = 6;
+            this.lblPlayerAmmo.Text = "Player ammunition:";
+            // 
+            // lblEventLog
+            // 
+            this.lblEventLog.AutoSize = true;
+            this.lblEventLog.Location = new System.Drawing.Point(210, 31);
+            this.lblEventLog.Name = "lblEventLog";
+            this.lblEventLog.Size = new System.Drawing.Size(52, 13);
+            this.lblEventLog.TabIndex = 7;
+            this.lblEventLog.Text = "Event log";
+            // 
+            // btnShotgun
+            // 
+            this.btnShotgun.Enabled = false;
+            this.btnShotgun.Location = new System.Drawing.Point(190, 125);
+            this.btnShotgun.Name = "btnShotgun";
+            this.btnShotgun.Size = new System.Drawing.Size(75, 23);
+            this.btnShotgun.TabIndex = 8;
+            this.btnShotgun.Text = "Shotgun!";
+            this.btnShotgun.UseVisualStyleBackColor = true;
+            this.btnShotgun.Click += new System.EventHandler(this.btnShotgun_Click);
+            // 
+            // frmShotgun
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.btnShotgun);
+            this.Controls.Add(this.lblEventLog);
+            this.Controls.Add(this.lblPlayerAmmo);
+            this.Controls.Add(this.lblComputerAmmo);
+            this.Controls.Add(this.lblComputerAmmoCount);
+            this.Controls.Add(this.lblPlayerAmmoCount);
+            this.Controls.Add(this.btnFire);
+            this.Controls.Add(this.btnBlock);
+            this.Controls.Add(this.btnLoad);
+            this.Name = "frmShotgun";
+            this.Text = "Shotgun";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.Button btnBlock;
+        private System.Windows.Forms.Button btnFire;
+        private System.Windows.Forms.Label lblPlayerAmmoCount;
+        private System.Windows.Forms.Label lblComputerAmmoCount;
+        private System.Windows.Forms.Label lblComputerAmmo;
+        private System.Windows.Forms.Label lblPlayerAmmo;
+        private System.Windows.Forms.Label lblEventLog;
+        private System.Windows.Forms.Button btnShotgun;
     }
 }
 
--- shotgun/Form1.cs
+++ shotgun/Form1.cs
@@ -10,11 +10,111 @@ using System.Windows.Forms;
 
 namespace shotgun
 {
-    public partial class Form1 : Form
+    public partial class frmShotgun : Form
     {
-        public Form1()
+
+        int playerAmmo = 0, computerAmmo = 0;
+
+        private string ComputerTurn()
+        {
+            string computerAction = "";
+
+            //ska kolla vilka möjligheter datorn har, sedan randomisera en action att utföra
+
+            return computerAction;
+        }
+
+        private string ResolveTurn(string playerAction, string computerAction)
+        {
+            string result = "";
+
+            if (playerAction == "load")
+            {
+                switch (computerAction)
+                {
+                    case "fire": result = "computer win"; break;
+                    case "shotgun": result = "computer win"; break;
+                    default: result = "draw"; break;
+                }
+            }
+            else if (playerAction == "block")
+            {
+                switch (computerAction)
+                {
+                    case "shotgun": result = "computer win"; break;
+                    default: result = "draw"; break;
+                }
+            }
+            else if (playerAction == "fire")
+            {
+                switch (computerAction)
+                {
+                    case "load": result = "player win"; break;
+                    case "shotgun": result = "computer win"; break;
+                    default: result = "draw"; break;
+                }
+            }
+            else if (playerAction == "shotgun")
+            {
+                switch (computerAction)
+                {
+                    case "shotgun": result = "both lose"; break;
+                    default: result = "player win"; break;
+                }
+            }
+
+
+            return result;
+        }
+
+        private void PlayerHasShotgun()
+        {
+            btnLoad.Enabled = false;
+            btnBlock.Enabled = false;
+            btnFire.Enabled = false;
+            btnShotgun.Enabled = true;
+        }
+        
+        private void PlayerOutOfAmmo()
+        {
+            btnFire.Enabled = false;
+        }
+
+        public frmShotgun()
         {
             InitializeComponent();
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            btnFire.Enabled = true;
+            playerAmmo++;
+
+            if (playerAmmo == 0)
+            {
+                PlayerOutOfAmmo();
+            }
+            else if (playerAmmo == 3)
+            {
+                PlayerHasShotgun();
+            }
+
+            ResolveTurn("fire", ComputerTurn()); //returnerar just nu en string till inget
+        }
+
+        private void btnBlock_Click(object sender, EventArgs e)
+        {
+            ResolveTurn("block", ComputerTurn());
+        }
+
+        private void btnFire_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnShotgun_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
